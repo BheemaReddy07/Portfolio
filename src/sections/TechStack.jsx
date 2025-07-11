62f1f5dc65e8e1f6dfd6a03f332e5b026a8706fe
BheemaReddy07 <bheemareddy2910@gmail.com>
2025-07-11 14:53:23 +0530

    hidden techstack

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -12,7 +12,7 @@ const TechStack = () => {
 
 
   return (
-    <div id="techstack" className="flex-center section-padding">
+    <div id="techstack" className="hidden md:flex  justify-center items-center px-5 md:px-10 md:mt-40 mt-20">
       <div className="w-full h-full md:px-10 px-5">
         <h2 className="text-center font-bold text-5xl">
           My preferred Tech Stack
